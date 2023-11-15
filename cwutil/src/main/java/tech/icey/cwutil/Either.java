c6adc257238bfd2e72876262f422c3a4a578ad92
Chuigda WhiteGive <icey@icey.tech>
2023-11-15 16:03:33 +0800

    No hair, no tie, hiding in thy <del>Wi-Fi</del> ADSL!

--- cwutil/src/main/java/tech/icey/cwutil/Either.java
+++ cwutil/src/main/java/tech/icey/cwutil/Either.java
@@ -1,6 +1,14 @@
 package tech.icey.cwutil;
 
-public abstract sealed class Either<L, R> {}
+public abstract sealed class Either<L, R> {
+    public static <L, R> Either<L, R> left(L value) {
+        return new Left<>(value);
+    }
+
+    public static <L, R> Either<L, R> right(R value) {
+        return new Right<>(value);
+    }
+}
 
 final class Left<L, R> extends Either<L, R> {
     public final L value;
--- cwutil/src/main/java/tech/icey/rfc6455/Connection.java
+++ cwutil/src/main/java/tech/icey/rfc6455/Connection.java
@@ -2,11 +2,14 @@ package tech.icey.rfc6455;
 
 import tech.icey.cwutil.Either;
 import tech.icey.cwutil.NotNull;
+import tech.icey.cwutil.Nullable;
+import tech.icey.cwutil.Tuple3;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 public final class Connection implements AutoCloseable {
@@ -25,39 +28,12 @@ public final class Connection implements AutoCloseable {
         this.isClient = isClient;
     }
 
-    private void impWrite(OpCode opCode, @NotNull byte[] bytes) throws IOException {
+    private void impWrite(OpCode opCode, @Nullable byte[] bytes) throws IOException {
         boolean hasMask = isClient;
         int maskBit = hasMask ? 0x80 : 0x00;
         byte controlByte = (byte)(0x80 | opCode.getCode());
-        int payloadLength = bytes.length;
 
-        if (payloadLength <= 125) {
-            tx.write(new byte[] { controlByte, (byte)(payloadLength | maskBit) });
-        } else if (payloadLength <= 0xFFFF) {
-            tx.write(new byte[] {
-                    controlByte,
-                    (byte)(126 | maskBit),
-                    (byte)((payloadLength >> 8) & 0xFF),
-                    (byte)(payloadLength & 0xFF)
-            });
-        } else {
-            tx.write(new byte[] {
-                    controlByte,
-                    (byte)(127 | maskBit),
-                    // length is only 32 bits, so fill the rest with 0
-                    0, 0, 0, 0,
-                    (byte)((payloadLength >> 24) & 0xFF),
-                    (byte)((payloadLength >> 16) & 0xFF),
-                    (byte)((payloadLength >> 8) & 0xFF),
-                    (byte)(payloadLength & 0xFF)
-            });
-        }
-
-        if (bytes.length == 0) {
-            return;
-        }
-
-        if (hasMask) {
+        if (bytes != null && bytes.length != 0 && hasMask) {
             int maskingKey = (int)(Math.random() * 0xFFFFFFFF);
             byte[] maskingKeyBytes = new byte[] {
                     (byte)((maskingKey >> 24) & 0xFF),
@@ -72,7 +48,114 @@ public final class Connection implements AutoCloseable {
             }
         }
 
-        tx.write(bytes);
+        int payloadLength = bytes == null ? 0 : bytes.length;
+
+        synchronized (tx) {
+            if (payloadLength <= 125) {
+                    tx.write(new byte[]{controlByte, (byte) (payloadLength | maskBit)});
+            } else if (payloadLength <= 0xFFFF) {
+                tx.write(new byte[] {
+                        controlByte,
+                        (byte)(126 | maskBit),
+                        (byte)((payloadLength >> 8) & 0xFF),
+                        (byte)(payloadLength & 0xFF)
+                });
+            } else {
+                tx.write(new byte[] {
+                        controlByte,
+                        (byte)(127 | maskBit),
+                        0, 0, 0, 0,
+                        (byte)((payloadLength >> 24) & 0xFF),
+                        (byte)((payloadLength >> 16) & 0xFF),
+                        (byte)((payloadLength >> 8) & 0xFF),
+                        (byte)(payloadLength & 0xFF)
+                });
+            }
+
+            if (bytes == null || bytes.length == 0) {
+                return;
+            }
+
+            tx.write(bytes);
+        }
+    }
+
+    private @NotNull Tuple3<OpCode, byte[], Boolean> readFragment() throws IOException {
+        byte[] header = new byte[2];
+        if (rx.read(header) < 2) {
+            throw new IOException("Unexpected EOF");
+        }
+
+        byte controlByte = header[0];
+        if ((controlByte & (0x60 | 0x40 | 0x20)) != 0) {
+            throw new IOException("Invalid RFC6455 frame: reserved bits set");
+        }
+
+        boolean fin = (controlByte & 0x80) != 0;
+        byte opCodeByte = (byte)(controlByte & 0x0F);
+        if (opCodeByte != 0x0 &&
+                opCodeByte != 0x1 &&
+                opCodeByte != 0x2 &&
+                opCodeByte != 0x8 &&
+                opCodeByte != 0x9 &&
+                opCodeByte != 0xA) {
+            throw new IOException("Invalid RFC6455 frame: reserved op code");
+        }
+
+        boolean hasMask = (header[1] & 0x80) != 0;
+        int payloadLength = header[1] & 0x7F;
+
+        if (payloadLength == 126) {
+            byte[] lengthBytes = new byte[2];
+            if (rx.read(lengthBytes) < 2) {
+                throw new IOException("Unexpected EOF");
+            }
+            payloadLength = ((lengthBytes[0] & 0xFF) << 8) | (lengthBytes[1] & 0xFF);
+        } else if (payloadLength == 127) {
+            byte[] lengthBytes = new byte[8];
+            if (rx.read(lengthBytes) < 8) {
+                throw new IOException("Unexpected EOF");
+            }
+
+            payloadLength = payloadLengthFrom8Bytes(lengthBytes);
+        }
+
+        byte[] maskingKeyBytes = null;
+        if (hasMask) {
+            maskingKeyBytes = new byte[4];
+            if (rx.read(maskingKeyBytes) < 4) {
+                throw new IOException("Unexpected EOF");
+            }
+        }
+
+        byte[] payload = new byte[payloadLength];
+        if (rx.read(payload) < payloadLength) {
+            throw new IOException("Unexpected EOF");
+        }
+
+        if (hasMask) {
+            for (int i = 0; i < payloadLength; i++) {
+                payload[i] ^= maskingKeyBytes[i % 4];
+            }
+        }
+
+        return new Tuple3<>(OpCode.values()[opCodeByte], payload, fin);
+    }
+
+    private static int payloadLengthFrom8Bytes(byte[] lengthBytes) throws IOException {
+        long payloadLengthLong = ((long) (lengthBytes[0] & 0xFF) << 56) |
+                ((long) (lengthBytes[1] & 0xFF) << 48) |
+                ((long) (lengthBytes[2] & 0xFF) << 40) |
+                ((long) (lengthBytes[3] & 0xFF) << 32) |
+                ((long) (lengthBytes[4] & 0xFF) << 24) |
+                ((lengthBytes[5] & 0xFF) << 16) |
+                ((lengthBytes[6] & 0xFF) << 8) |
+                (lengthBytes[7] & 0xFF);
+
+        if (payloadLengthLong > Integer.MAX_VALUE) {
+            throw new IOException("Payload length too large");
+        }
+        return (int)payloadLengthLong;
     }
 
     public enum OpCode {
@@ -105,9 +188,58 @@ public final class Connection implements AutoCloseable {
         impWrite(OpCode.TEXT, utf8Bytes);
     }
 
-    public @NotNull Either<byte[], String> read() throws IOException {
-        // TODO: implement reading logics
-        return null;
+    public @Nullable Either<byte[], String> read() throws IOException {
+        while (true) {
+            var fragmentResult = readFragment();
+            OpCode opCode = fragmentResult.first;
+            byte[] payload = fragmentResult.second;
+            boolean fin = fragmentResult.third;
+
+            ByteBuffer buffer = payload != null ? ByteBuffer.wrap(payload) : null;
+            switch (opCode) {
+                case PING -> impWrite(OpCode.PONG, payload);
+                case PONG -> {}
+                case CLOSE -> {
+                    try {
+                        impWrite(OpCode.CLOSE, null);
+                    } catch (IOException ignored) {
+                        // ignore any exception when we're already going to close
+                    }
+                    return null;
+                }
+                case TEXT, BINARY -> {
+                    while (!fin) {
+                        fragmentResult = readFragment();
+                        OpCode contOpCode = fragmentResult.first;
+                        payload = fragmentResult.second;
+                        fin = fragmentResult.third;
+
+                        if (contOpCode != OpCode.CONTINUATION) {
+                            throw new IOException("Invalid RFC6455 frame: continuation expected");
+                        }
+
+                        if (buffer == null) {
+                            buffer = ByteBuffer.wrap(payload);
+                        } else {
+                            buffer.put(payload);
+                        }
+                    }
+
+                    if (buffer == null) {
+                        return null;
+                    }
+
+                    if (opCode == OpCode.TEXT) {
+                        return Either.right(StandardCharsets.UTF_8.decode(buffer).toString());
+                    } else {
+                        byte[] bytes = new byte[buffer.position()];
+                        buffer.flip();
+                        buffer.get(bytes);
+                        return Either.left(bytes);
+                    }
+                }
+            }
+        }
     }
 
     @Override
