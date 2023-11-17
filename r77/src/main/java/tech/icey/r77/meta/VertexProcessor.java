b0bed7f7583cbc16a6baa358a79702ec6965956d
Chuigda WhiteGive <icey@icey.tech>
2023-11-17 14:26:35 +0800

    Testing this is not very easy yet, would require more effort

--- r77/src/main/java/tech/icey/r77/meta/VertexProcessor.java
+++ r77/src/main/java/tech/icey/r77/meta/VertexProcessor.java
@@ -5,7 +5,6 @@ import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
-import javax.tools.Diagnostic;
 import java.util.List;
 import java.util.Set;
 
@@ -16,16 +15,35 @@ public class VertexProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         for (TypeElement annotation : annotations) {
-            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Processing " + annotation);
+            System.out.println(annotation.getQualifiedName());
 
             Set<? extends Element> annotatedElements = roundEnv.getElementsAnnotatedWith(annotation);
-            List<? extends Element> fields = annotatedElements
+            List<TypeElement> classes = annotatedElements
                     .stream()
+                    .map(element -> (TypeElement) element)
                     .sorted((o1, o2) -> {
                         Attribute a1 = o1.getAnnotation(Attribute.class);
                         Attribute a2 = o2.getAnnotation(Attribute.class);
                         return a1.position() - a2.position();
                     }).toList();
+
+            for (TypeElement clazz : classes) {
+                // read all fields with annotation "Attribute"
+                // and generate code
+                System.out.println(clazz.getQualifiedName());
+                for (Element e : clazz.getEnclosedElements()) {
+                    Attribute attribute = e.getAnnotation(Attribute.class);
+                    if (attribute != null) {
+                        System.out.println("\tfield: @Attribute(name = \"" +
+                                attribute.name() +
+                                "\", position = " +
+                                attribute.position() +
+                                ") " +
+                                e.getSimpleName()
+                        );
+                    }
+                }
+            }
         }
 
         return false;
