0f3d7e488165ba5f2b529f0c9ddc0155d005a431
Chuigda WhiteGive <icey@icey.tech>
2023-12-04 13:08:12 +0800

    Of very good use

--- wgx/src/main/java/tech/icey/wgx/Drill.java
+++ wgx/src/main/java/tech/icey/wgx/Drill.java
@@ -4,5 +4,7 @@ public class Drill {
     public static void main(String[] args) {
         DeviceInfoDialog w = new DeviceInfoDialog(null, null);
         w.setVisible(true);
+
+        System.err.println("This will be displayed only after the dialog is closed");
     }
 }
