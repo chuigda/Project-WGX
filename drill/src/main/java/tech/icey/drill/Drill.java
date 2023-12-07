504ffd78fd191c944c561cbe63efccee31f7ec10
Rebuild <admin@rebuild.moe>
2023-12-06 23:20:04 -0500

    update

--- drill/src/main/java/tech/icey/drill/Drill.java
+++ drill/src/main/java/tech/icey/drill/Drill.java
@@ -2,16 +2,26 @@ package tech.icey.drill;
 
 import tech.icey.rfc6455.Client;
 import tech.icey.rfc6455.Connection;
+import tech.icey.rfc6455.RFC6455Callback;
+import tech.icey.util.Either;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class Drill {
     public static void main(String[] args) {
-        try (Connection websocketConnection = Client.connect("127.0.0.1", 6455, "/")) {
-            System.out.println(Objects.requireNonNull(websocketConnection.read()).right());
+        try (Connection websocketConnection = Client.connect("127.0.0.1", 6455, "/", new RFC6455Callback() {
+            @Override
+            public void onData(Either<byte[], String> data) {
+                System.out.println(Objects.requireNonNull(data).right());
+            }
+
+            @Override
+            public void onError(Exception e) {
+                e.printStackTrace();
+            }
+        })) {
             websocketConnection.write("告诉你一个生活开心的秘诀：少责备自己，多辱骂别人。被你骂的人都罪有应得！但别人骂你肯定是没素质");
-            System.out.println(Objects.requireNonNull(websocketConnection.read()).right());
         } catch (IOException e) {
             e.printStackTrace();
         }
