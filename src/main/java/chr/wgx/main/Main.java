d0e75a71c3b95dd89f300cb33ab32c601c21927b
Chuigda Whitegive <icey@icey.tech>
2024-11-18 02:17:57 +0800

    try JUL

--- src/main/java/chr/wgx/main/Main.java
+++ src/main/java/chr/wgx/main/Main.java
@@ -1,7 +1,17 @@
 package chr.wgx.main;
 
+import java.util.logging.Logger;
+
 public final class Main {
     public static void main(String[] args) {
+        logger.info("application started");
         Bootload.loadNativeLibraries();
+        logger.info("native libraries loaded");
     }
+
+    static {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$tFT%1$tT] [%4$-7s] %5$s %n");
+    }
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
 }
