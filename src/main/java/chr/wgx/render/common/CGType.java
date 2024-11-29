0d973d6418950c37c15137c451d49c09b478a6ee
HoshinoTented <hoshinotented@qq.com>
2024-11-29 21:16:53 +0800

    gl: trying to impl createPipeline correctly without testing

--- src/main/java/chr/wgx/render/common/CGType.java
+++ src/main/java/chr/wgx/render/common/CGType.java
@@ -4,13 +4,13 @@ import tech.icey.panama.annotation.enumtype;
 import tech.icey.vk4j.enumtype.VkFormat;
 
 public enum CGType {
-    Float (4,         4,  4, VkFormat.VK_FORMAT_R32_SFLOAT),
-    Vec2  (4 * 2,     4,  8, VkFormat.VK_FORMAT_R32G32_SFLOAT),
-    Vec3  (4 * 3,     4, 16, VkFormat.VK_FORMAT_R32G32B32_SFLOAT),
-    Vec4  (4 * 4,     4, 16, VkFormat.VK_FORMAT_R32G32B32A32_SFLOAT),
-    Mat2  (2 * 2 * 4, 4, 16, VkFormat.VK_FORMAT_R32G32_SFLOAT),
-    Mat3  (3 * 3 * 4, 4, 16, VkFormat.VK_FORMAT_R32G32B32_SFLOAT),
-    Mat4  (4 * 4 * 4, 4, 16, VkFormat.VK_FORMAT_R32G32B32A32_SFLOAT);
+    Float (4,         4,  4, VkFormat.VK_FORMAT_R32_SFLOAT, 1),
+    Vec2  (4 * 2,     4,  8, VkFormat.VK_FORMAT_R32G32_SFLOAT, 1),
+    Vec3  (4 * 3,     4, 16, VkFormat.VK_FORMAT_R32G32B32_SFLOAT, 1),
+    Vec4  (4 * 4,     4, 16, VkFormat.VK_FORMAT_R32G32B32A32_SFLOAT, 1),
+    Mat2  (2 * 2 * 4, 4, 16, VkFormat.VK_FORMAT_R32G32_SFLOAT, 2),
+    Mat3  (3 * 3 * 4, 4, 16, VkFormat.VK_FORMAT_R32G32B32_SFLOAT, 3),
+    Mat4  (4 * 4 * 4, 4, 16, VkFormat.VK_FORMAT_R32G32B32A32_SFLOAT, 4);
 
     /// 数据类型在 CPU 中存储时消耗的字节数
     public final int byteSize;
@@ -20,11 +20,14 @@ public enum CGType {
     public final int std140Alignment;
     /// 对应的 Vulkan format
     public final @enumtype(VkFormat.class) int vkFormat;
+    /// 在 GLES 中占用的 index 数量
+    public final int glIndexSize;
 
-    CGType(int byteSize, int alignment, int std140Alignment, @enumtype(VkFormat.class) int vkFormat) {
+    CGType(int byteSize, int alignment, int std140Alignment, @enumtype(VkFormat.class) int vkFormat, int glIndexSize) {
         this.byteSize = byteSize;
         this.alignment = alignment;
         this.std140Alignment = std140Alignment;
         this.vkFormat = vkFormat;
+        this.glIndexSize = glIndexSize;
     }
 }
--- src/main/java/chr/wgx/render/gles2/GLES2RenderEngine.java
+++ src/main/java/chr/wgx/render/gles2/GLES2RenderEngine.java
@@ -8,7 +8,9 @@ import tech.icey.gles2.GLES2;
 import tech.icey.gles2.GLES2Constants;
 import tech.icey.glfw.GLFW;
 import tech.icey.glfw.handle.GLFWwindow;
+import tech.icey.panama.annotation.enumtype;
 import tech.icey.panama.buffer.ByteBuffer;
+import tech.icey.panama.buffer.PointerBuffer;
 import tech.icey.xjbutil.container.Option;
 import tech.icey.xjbutil.container.Pair;
 import tech.icey.xjbutil.functional.Action0;
@@ -17,9 +19,22 @@ import tech.icey.xjbutil.functional.Action2;
 import tech.icey.xjbutil.sync.Oneshot;
 
 import java.lang.foreign.Arena;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentMap;
 
 public final class GLES2RenderEngine extends AbstractRenderEngine {
+    // TODO: move this to elsewhere
+    @FunctionalInterface
+    public interface CheckedAction {
+        void invoke(GLES2 gl, Arena arena) throws RenderException;
+    }
+
+    private final ConcurrentLinkedQueue<CheckedAction> taskQueue = new ConcurrentLinkedQueue<>();
+    private final ConcurrentMap<Long, Object> handlerMapping = new ConcurrentHashMap<>();
+
     public GLES2RenderEngine(
             Action1<AbstractRenderEngine> onInit,
             Action2<Integer, Integer> onResize,
@@ -66,6 +81,10 @@ public final class GLES2RenderEngine extends AbstractRenderEngine {
         // there's actually nothing to do here
     }
 
+    private void invokeLater(CheckedAction run) {
+        taskQueue.add(run);
+    }
+
     @Override
     public ObjectHandle createObject(ObjectCreateInfo info) throws RenderException {
         return null;
@@ -73,7 +92,12 @@ public final class GLES2RenderEngine extends AbstractRenderEngine {
 
     @Override
     public List<ObjectHandle> createObject(List<ObjectCreateInfo> infos) throws RenderException {
-        return null;
+        var results = new ArrayList<ObjectHandle>();
+        for (var info : infos) {
+            results.add(createObject(info));        // null implies exception, no need to check
+        }
+
+        return results;
     }
 
     @Override
@@ -101,9 +125,53 @@ public final class GLES2RenderEngine extends AbstractRenderEngine {
         return null;
     }
 
+    private static int loadShader(Arena arena, GLES2 gl, @enumtype(GLES2Constants.class) int shaderType, String shaderSource) {
+        var shaderHandle = gl.glCreateShader(shaderType);
+        gl.glShaderSource(shaderHandle, 1, new PointerBuffer(arena.allocateFrom(shaderSource)), null);
+        gl.glCompileShader(shaderHandle);
+
+        // TODO: check shader compilation status
+
+        return shaderHandle;
+    }
+
+    private static void bindAttributes(GLES2 gl, Arena arena, int programHandle, VertexInputInfo info) {
+        int index = 0;
+        for (var attr : info.attributes) {
+            var ty = attr.type;
+            var name = attr.name;
+            gl.glBindAttribLocation(programHandle, index, ByteBuffer.allocateString(arena, name));
+            index = index + ty.glIndexSize;
+        }
+    }
+
     @Override
     public RenderPipelineHandle createPipeline(RenderPipelineCreateInfo info) throws RenderException {
-        return null;
+        var handle = nextHandle();
+        invokeLater((gles, arena) -> {
+            if (! (info.gles2ShaderProgram instanceof Option.Some<ShaderProgram.GLES2> someProgramSource)) {
+                // TODO: do something
+                throw new RuntimeException();
+            }
+
+            var programSource = someProgramSource.value;
+            var vertexShader = programSource.vertexShader;
+            var fragmentShader = programSource.fragmentShader;
+
+            var programHandle = gles.glCreateProgram();
+
+            // attributes
+            bindAttributes(gles, arena, programHandle, info.vertexInputInfo);
+
+            // shaders
+            gles.glAttachShader(programHandle, loadShader(arena, gles, GLES2Constants.GL_VERTEX_SHADER, vertexShader));
+            gles.glAttachShader(programHandle, loadShader(arena, gles, GLES2Constants.GL_FRAGMENT_SHADER, fragmentShader));
+            gles.glLinkProgram(programHandle);
+
+            // TODO: check program compilation status
+        });
+
+        return new RenderPipelineHandle(handle);
     }
 
     @Override
