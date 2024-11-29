59ca1e7db2f559bdaf14c3d42e939aef93235a50
Chuigda Whitegive <icey@icey.tech>
2024-11-29 17:31:50 +0800

    `ClearBehavior`

--- src/main/java/chr/wgx/render/info/RenderTaskInfo.java
+++ src/main/java/chr/wgx/render/info/RenderTaskInfo.java
@@ -1,5 +1,7 @@
 package chr.wgx.render.info;
 
+import chr.wgx.render.common.ClearBehavior;
+import chr.wgx.render.common.Color;
 import chr.wgx.render.handle.ColorAttachmentHandle;
 import chr.wgx.render.handle.DepthAttachmentHandle;
 import chr.wgx.render.handle.ObjectHandle;
@@ -15,7 +17,45 @@ public final class RenderTaskInfo {
     public final List<ColorAttachmentHandle> colorAttachments;
     public final Option<DepthAttachmentHandle> depthAttachment;
 
+    public final ClearBehavior clearBehavior;
+    public final Option<Color> clearColor;
+
     // TODO: attachments, uniforms and push constants
+
+    public RenderTaskInfo(
+            RenderPipelineHandle pipelineHandle,
+            List<ObjectHandle> objectHandles,
+            List<ColorAttachmentHandle> colorAttachments,
+            Option<DepthAttachmentHandle> depthAttachment,
+            ClearBehavior clearBehavior,
+            Option<Color> clearColor
+    ) {
+        this.pipelineHandle = pipelineHandle;
+        this.objectHandles = objectHandles;
+        this.colorAttachments = colorAttachments;
+        this.depthAttachment = depthAttachment;
+        this.clearBehavior = clearBehavior;
+        this.clearColor = clearColor;
+    }
+
+    public RenderTaskInfo(
+            RenderPipelineHandle pipelineHandle,
+            List<ObjectHandle> objectHandles,
+            List<ColorAttachmentHandle> colorAttachments,
+            Option<DepthAttachmentHandle> depthAttachment,
+            ClearBehavior clearBehavior
+    ) {
+        this(
+                pipelineHandle,
+                objectHandles,
+                colorAttachments,
+                depthAttachment,
+                clearBehavior,
+                clearBehavior == ClearBehavior.DONT_CLEAR ?
+                        Option.none()
+                        : Option.some(new Color(0.0f, 0.0f, 0.0f, 0.0f))
+        );
+    }
 
     public RenderTaskInfo(
             RenderPipelineHandle pipelineHandle,
@@ -23,9 +63,13 @@ public final class RenderTaskInfo {
             List<ColorAttachmentHandle> colorAttachments,
             Option<DepthAttachmentHandle> depthAttachment
     ) {
-        this.pipelineHandle = pipelineHandle;
-        this.objectHandles = objectHandles;
-        this.colorAttachments = colorAttachments;
-        this.depthAttachment = depthAttachment;
+        this(
+                pipelineHandle,
+                objectHandles,
+                colorAttachments,
+                depthAttachment,
+                ClearBehavior.DONT_CLEAR,
+                Option.none()
+        );
     }
 }
