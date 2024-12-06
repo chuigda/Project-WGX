2bd6502b90cc309d46fcd9f5b950faaf261ec824
Chuigda Whitegive <icey@icey.tech>
2024-12-07 02:31:40 +0800

    Full interaction

--- src/main/java/chr/wgx/util/gltf/GLTFCollection.java
+++ src/main/java/chr/wgx/util/gltf/GLTFCollection.java
@@ -3,22 +3,36 @@ package chr.wgx.util.gltf;
 import tech.icey.panama.IPointer;
 import tech.icey.panama.NativeLayout;
 
+import java.lang.foreign.AddressLayout;
 import java.lang.foreign.MemoryLayout;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class GLTFCollection implements IPointer, AutoCloseable {
-    private static final MemoryLayout LAYOUT = NativeLayout.structLayout(
-            ValueLayout.ADDRESS.withTargetLayout(GLTFMesh.LAYOUT), // models: *mut GLTFMesh
-            ValueLayout.JAVA_LONG // model_count: u64
-    );
-
     private final GLTF gltf;
     public final MemorySegment segment;
 
     GLTFCollection(GLTF gltf, MemorySegment segment) {
         this.gltf = gltf;
         this.segment = segment;
+    }
+
+    public long modelCount() {
+        return segment.get(LAYOUT$model_count, OFFSET$model_count);
+    }
+
+    public List<GLTFMesh> models() {
+        long count = modelCount();
+
+        MemorySegment ptr = segment.get(LAYOUT$models, OFFSET$models).reinterpret(count * GLTFMesh.SIZE);
+        List<GLTFMesh> ret = new ArrayList<>();
+        for (long i = 0; i < count; i++) {
+            MemorySegment modelSegment = ptr.asSlice(i * GLTFMesh.SIZE, GLTFMesh.SIZE);
+            ret.add(new GLTFMesh(modelSegment));
+        }
+        return ret;
     }
 
     @Override
@@ -30,4 +44,19 @@ public final class GLTFCollection implements IPointer, AutoCloseable {
     public void close() {
         gltf.free(this);
     }
+
+    static final MemoryLayout LAYOUT = NativeLayout.structLayout(
+            ValueLayout.ADDRESS.withTargetLayout(GLTFMesh.LAYOUT).withName("models"), // models: *mut GLTFMesh
+            ValueLayout.JAVA_LONG.withName("model_count") // model_count: u64
+    );
+    static final long SIZE = LAYOUT.byteSize();
+
+    private static final MemoryLayout.PathElement PATH$models = MemoryLayout.PathElement.groupElement("sType");
+    private static final MemoryLayout.PathElement PATH$model_count = MemoryLayout.PathElement.groupElement("pNext");
+
+    private static final AddressLayout LAYOUT$models = (AddressLayout) LAYOUT.select(PATH$models);
+    private static final ValueLayout.OfLong LAYOUT$model_count = (ValueLayout.OfLong) LAYOUT.select(PATH$model_count);
+
+    private static final long OFFSET$models = LAYOUT.byteOffset(PATH$models);
+    private static final long OFFSET$model_count = LAYOUT.byteOffset(PATH$model_count);
 }
--- src/main/java/chr/wgx/util/gltf/GLTFVertex.java
+++ src/main/java/chr/wgx/util/gltf/GLTFVertex.java
@@ -9,9 +9,10 @@ import java.lang.foreign.ValueLayout;
 
 public record GLTFVertex(MemorySegment segment) implements IPointer {
     static final MemoryLayout LAYOUT = NativeLayout.structLayout(
-            MemoryLayout.sequenceLayout(3, ValueLayout.JAVA_FLOAT), // position: [f32; 3]
-            MemoryLayout.sequenceLayout(3, ValueLayout.JAVA_FLOAT), // normal: [f32; 3]
-            MemoryLayout.sequenceLayout(2, ValueLayout.JAVA_FLOAT), // tex_coord: [f32; 2]
-            MemoryLayout.sequenceLayout(1, ValueLayout.JAVA_INT)  // id: u32
+            MemoryLayout.sequenceLayout(3, ValueLayout.JAVA_FLOAT).withName("position"), // position: [f32; 3]
+            MemoryLayout.sequenceLayout(3, ValueLayout.JAVA_FLOAT).withName("normal"), // normal: [f32; 3]
+            MemoryLayout.sequenceLayout(2, ValueLayout.JAVA_FLOAT).withName("tex_coord"), // tex_coord: [f32; 2]
+            MemoryLayout.sequenceLayout(1, ValueLayout.JAVA_INT).withName("id")  // id: u32
     );
+    static final long SIZE = LAYOUT.byteSize();
 }
