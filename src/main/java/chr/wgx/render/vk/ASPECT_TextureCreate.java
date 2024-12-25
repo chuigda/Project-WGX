d84b19600bc5e2b1dad15ae3b926bb8d4552cd06
Chuigda Whitegive <icey@icey.tech>
2024-12-26 04:54:28 +0800

    Two-queue'd uploading ok

--- src/main/java/chr/wgx/render/vk/ASPECT_TextureCreate.java
+++ src/main/java/chr/wgx/render/vk/ASPECT_TextureCreate.java
@@ -93,25 +93,93 @@ public final class ASPECT_TextureCreate {
             }
 
             if (cx.dedicatedTransferQueue.isSome()) {
-                throw new UnsupportedOperationException("暂不支持专用传输队列");
+                cx.executeTransferCommand(cmdBuf -> {
+                    VkImageMemoryBarrier[] barriers = VkImageMemoryBarrier.allocate(arena, infoList.size());
+                    for (int i = 0; i < infoList.size(); i++) {
+                        Resource.Image image = imageList.get(i);
+                        VkImageMemoryBarrier barrier = barriers[i];
+
+                        imageLayout_undefinedToTransferDest(
+                                image,
+                                barrier,
+                                Constants.VK_QUEUE_FAMILY_IGNORED,
+                                Constants.VK_QUEUE_FAMILY_IGNORED
+                        );
+                    }
+                    cx.dCmd.vkCmdPipelineBarrier(
+                            cmdBuf,
+                            VkPipelineStageFlags.VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
+                            VkPipelineStageFlags.VK_PIPELINE_STAGE_TRANSFER_BIT,
+                            VkDependencyFlags.VK_DEPENDENCY_BY_REGION_BIT,
+                            0, null,
+                            0, null,
+                            infoList.size(), barriers[0]
+                    );
+
+                    VkBufferImageCopy copyRegion = VkBufferImageCopy.allocate(arena);
+                    VkImageSubresourceLayers imageSubresource = copyRegion.imageSubresource();
+                    imageSubresource.aspectMask(VkImageAspectFlags.VK_IMAGE_ASPECT_COLOR_BIT);
+                    imageSubresource.mipLevel(0);
+                    imageSubresource.baseArrayLayer(0);
+                    imageSubresource.layerCount(1);
+                    VkExtent3D extent = copyRegion.imageExtent();
+                    extent.depth(1);
+
+                    for (int i = 0; i < infoList.size(); i++) {
+                        TextureCreateInfo info = infoList.get(i);
+                        Resource.Buffer stagingBuffer = stagingBufferList.get(i);
+                        Resource.Image image = imageList.get(i);
+
+                        extent.width(info.image.getWidth());
+                        extent.height(info.image.getHeight());
+
+                        cx.dCmd.vkCmdCopyBufferToImage(
+                                cmdBuf,
+                                stagingBuffer.buffer,
+                                image.image,
+                                VkImageLayout.VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+                                1,
+                                copyRegion
+                        );
+                    }
+                });
+
+                cx.executeGraphicsCommand(cmdBuf -> {
+                    VkImageMemoryBarrier[] barriers = VkImageMemoryBarrier.allocate(arena, infoList.size());
+                    for (int i = 0; i < infoList.size(); i++) {
+                        Resource.Image image = imageList.get(i);
+                        VkImageMemoryBarrier barrier = barriers[i];
+
+                        imageLayout_transferDestToSharedReadOnly(
+                                image,
+                                barrier,
+                                cx.dedicatedTransferQueueFamilyIndex.get(),
+                                cx.graphicsQueueFamilyIndex
+                        );
+                    }
+                    cx.dCmd.vkCmdPipelineBarrier(
+                            cmdBuf,
+                            VkPipelineStageFlags.VK_PIPELINE_STAGE_TRANSFER_BIT,
+                            VkPipelineStageFlags.VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
+                            VkDependencyFlags.VK_DEPENDENCY_BY_REGION_BIT,
+                            0, null,
+                            0, null,
+                            infoList.size(), barriers[0]
+                    );
+                });
             } else {
                 cx.executeGraphicsCommand(cmdBuf -> {
                     VkImageMemoryBarrier[] barriers = VkImageMemoryBarrier.allocate(arena, infoList.size());
                     for (int i = 0; i < infoList.size(); i++) {
                         Resource.Image image = imageList.get(i);
                         VkImageMemoryBarrier barrier = barriers[i];
-                        barrier.srcAccessMask(0);
-                        barrier.dstAccessMask(VkAccessFlags.VK_ACCESS_TRANSFER_WRITE_BIT);
-                        barrier.oldLayout(VkImageLayout.VK_IMAGE_LAYOUT_UNDEFINED);
-                        barrier.newLayout(VkImageLayout.VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
-                        barrier.srcQueueFamilyIndex(Constants.VK_QUEUE_FAMILY_IGNORED);
-                        barrier.dstQueueFamilyIndex(Constants.VK_QUEUE_FAMILY_IGNORED);
-                        barrier.image(image.image);
-                        barrier.subresourceRange().aspectMask(VkImageAspectFlags.VK_IMAGE_ASPECT_COLOR_BIT);
-                        barrier.subresourceRange().baseMipLevel(0);
-                        barrier.subresourceRange().levelCount(1);
-                        barrier.subresourceRange().baseArrayLayer(0);
-                        barrier.subresourceRange().layerCount(1);
+
+                        imageLayout_undefinedToTransferDest(
+                                image,
+                                barrier,
+                                Constants.VK_QUEUE_FAMILY_IGNORED,
+                                Constants.VK_QUEUE_FAMILY_IGNORED
+                        );
                     }
                     cx.dCmd.vkCmdPipelineBarrier(
                             cmdBuf,
@@ -153,18 +221,13 @@ public final class ASPECT_TextureCreate {
                     for (int i = 0; i < infoList.size(); i++) {
                         Resource.Image image = imageList.get(i);
                         VkImageMemoryBarrier barrier = barriers[i];
-                        barrier.srcAccessMask(0);
-                        barrier.dstAccessMask(VkAccessFlags.VK_ACCESS_SHADER_READ_BIT);
-                        barrier.oldLayout(VkImageLayout.VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
-                        barrier.newLayout(VkImageLayout.VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
-                        barrier.srcQueueFamilyIndex(Constants.VK_QUEUE_FAMILY_IGNORED);
-                        barrier.dstQueueFamilyIndex(Constants.VK_QUEUE_FAMILY_IGNORED);
-                        barrier.image(image.image);
-                        barrier.subresourceRange().aspectMask(VkImageAspectFlags.VK_IMAGE_ASPECT_COLOR_BIT);
-                        barrier.subresourceRange().baseMipLevel(0);
-                        barrier.subresourceRange().levelCount(1);
-                        barrier.subresourceRange().baseArrayLayer(0);
-                        barrier.subresourceRange().layerCount(1);
+
+                        imageLayout_transferDestToSharedReadOnly(
+                                image,
+                                barrier,
+                                Constants.VK_QUEUE_FAMILY_IGNORED,
+                                Constants.VK_QUEUE_FAMILY_IGNORED
+                        );
                     }
                     cx.dCmd.vkCmdPipelineBarrier(
                             cmdBuf,
@@ -201,6 +264,46 @@ public final class ASPECT_TextureCreate {
                 stagingBuffer.dispose(cx);
             }
         }
+    }
+
+    private void imageLayout_undefinedToTransferDest(
+            Resource.Image image,
+            VkImageMemoryBarrier barrier,
+            int srcQueueFamilyIndex,
+            int dstQueueFamilyIndex
+    ) {
+        barrier.srcAccessMask(0);
+        barrier.dstAccessMask(VkAccessFlags.VK_ACCESS_TRANSFER_WRITE_BIT);
+        barrier.oldLayout(VkImageLayout.VK_IMAGE_LAYOUT_UNDEFINED);
+        barrier.newLayout(VkImageLayout.VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+        barrier.srcQueueFamilyIndex(srcQueueFamilyIndex);
+        barrier.dstQueueFamilyIndex(dstQueueFamilyIndex);
+        barrier.image(image.image);
+        barrier.subresourceRange().aspectMask(VkImageAspectFlags.VK_IMAGE_ASPECT_COLOR_BIT);
+        barrier.subresourceRange().baseMipLevel(0);
+        barrier.subresourceRange().levelCount(1);
+        barrier.subresourceRange().baseArrayLayer(0);
+        barrier.subresourceRange().layerCount(1);
+    }
+
+    private void imageLayout_transferDestToSharedReadOnly(
+            Resource.Image image,
+            VkImageMemoryBarrier barrier,
+            int srcQueueFamilyIndex,
+            int dstQueueFamilyIndex
+    ) {
+        barrier.srcAccessMask(0);
+        barrier.dstAccessMask(VkAccessFlags.VK_ACCESS_SHADER_READ_BIT);
+        barrier.oldLayout(VkImageLayout.VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+        barrier.newLayout(VkImageLayout.VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+        barrier.srcQueueFamilyIndex(srcQueueFamilyIndex);
+        barrier.dstQueueFamilyIndex(dstQueueFamilyIndex);
+        barrier.image(image.image);
+        barrier.subresourceRange().aspectMask(VkImageAspectFlags.VK_IMAGE_ASPECT_COLOR_BIT);
+        barrier.subresourceRange().baseMipLevel(0);
+        barrier.subresourceRange().levelCount(1);
+        barrier.subresourceRange().baseArrayLayer(0);
+        barrier.subresourceRange().layerCount(1);
     }
 
     private final VulkanRenderEngine engine;
