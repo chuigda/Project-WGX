e7c9e5701ef7806172479f8a0eaa4311afe0f56b
Chuigda Whitegive <icey@icey.tech>
2024-12-31 08:29:16 +0800

    等建模师的 UV 过来我们就可以进行总装测试了

--- src/main/java/chr/wgx/render/common/PixelFormat.java
+++ src/main/java/chr/wgx/render/common/PixelFormat.java
@@ -14,7 +14,9 @@ public enum PixelFormat {
     /// 交换链图像使用的格式，使用对于 Vulkan 而言支持更加广泛的 BGRA 而不是 RGBA 排列
     RGBA_SWAPCHAIN(VkFormat.VK_FORMAT_B8G8R8A8_SRGB, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE),
     /// 单色纹理使用的格式
-    R32_OPTIMAL(VkFormat.VK_FORMAT_R32_SFLOAT, GL_R32F_EXT, GL_R32F_EXT, GL_FLOAT),
+    R32_FLOAT(VkFormat.VK_FORMAT_R32_SFLOAT, GL_R32F_EXT, GL_R32F_EXT, GL_FLOAT),
+    /// 用于存储 ID 类数据的纹理使用的格式；GLES2 不支持单通道无符号整数纹理，仍然使用浮点纹理
+    R32_UINT(VkFormat.VK_FORMAT_R32_UINT, GL_R32F_EXT, GL_R32F_EXT, GL_FLOAT),
     /// 深度缓冲附件使用的格式
     DEPTH_BUFFER_OPTIMAL(VK_FORMAT_D32_SFLOAT, GL_DEPTH_COMPONENT, GL_DEPTH_COMPONENT, GL_UNSIGNED_SHORT);
 
