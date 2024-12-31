4294462a42fc5401997797818dbb44c03d96f182
Chuigda Whitegive <icey@icey.tech>
2025-01-01 04:38:30 +0800

    Do not load vulkan libraries when using GLES2 mode

--- src/main/java/chr/wgx/main/RenderApplication.java
+++ src/main/java/chr/wgx/main/RenderApplication.java
@@ -46,20 +46,31 @@ public final class RenderApplication {
     }
 
     public static void loadNativeLibraries() {
-        logger.fine("正使用 tech.icey.vk4j.VulkanLoader 加载 vulkan 本地库");
-        VulkanLoader.loadVulkanLibrary();
+        Config config = Config.config();
+        boolean isVulkan = config.renderMode.equals("vulkan");
+
+        if (isVulkan) {
+            logger.fine("正使用 tech.icey.vk4j.VulkanLoader 加载 vulkan 本地库");
+            VulkanLoader.loadVulkanLibrary();
+        }
 
         if (System.getProperty("os.name").toLowerCase().contains("win")) {
             logger.fine("从资源 /lib/glfw3.dll 中加载 glfw");
             SharedObjectLoader.loadFromResources("/resources/lib/glfw3.dll", ".dll");
-            logger.fine("从资源 /lib/vma.dll 中加载 vma");
-            SharedObjectLoader.loadFromResources("/resources/lib/vma.dll", ".dll");
+
+            if (isVulkan) {
+                logger.fine("从资源 /lib/vma.dll 中加载 vma");
+                SharedObjectLoader.loadFromResources("/resources/lib/vma.dll", ".dll");
+            }
         }
         else {
             logger.fine("从系统环境中加载 glfw");
             System.loadLibrary("glfw");
-            logger.fine("从资源 /lib/libvma.so 中加载 vma");
-            SharedObjectLoader.loadFromResources("/resources/lib/libvma.so", ".so");
+
+            if (isVulkan) {
+                logger.fine("从资源 /lib/libvma.so 中加载 vma");
+                SharedObjectLoader.loadFromResources("/resources/lib/libvma.so", ".so");
+            }
         }
     }
 
